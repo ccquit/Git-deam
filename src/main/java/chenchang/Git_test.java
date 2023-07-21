00e99e59ba635a2b0391cfb653e73599d7f47ad6
chenchang <1527972485@qq.com>
2023-07-21 10:21:41 +0800

    first commit

---
+++ src/main/java/chenchang/Git_test.java
@@ -0,0 +1,12 @@
+/*作者： 陈畅
+ *时间：2023-07-21
+ *关于：
+ */
+
+package chenchang;
+
+public class Git_test {
+    public static void main(String[] args) {
+        System.out.println("hellow git");
+    }
+}
